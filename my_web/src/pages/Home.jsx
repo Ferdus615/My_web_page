d491aaa0d6cc9df4f768a5a9b7363b0722316148
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-05-09 01:54:32 +0600

    first commit react my_web_page

---
+++ my_web/src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import styles from "./home.module.css";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className={styles.container}>
+      <nav>
+        <img src="/FRK.svg" alt="logo" width="100px" />
+      </nav>
+      <div className={styles.intro_container}>
+        <h1 className={styles.intro}>
+          <span className={styles.greetings}>Hi, there I am</span>
+
+          <span className={styles.first_name}>Ferdus Rhaman</span>
+
+          <span className={styles.last_name}>Khan.</span>
+
+          <p className={styles.info}>
+            Programmer, <br />
+            Web Developer & <br />
+            Graphic Designer.
+          </p>
+        </h1>
+      </div>
+
+      <div className={styles.bottom_line}>
+        <div className={styles.location}>From Dhaka, Bangladsh</div>
+        <a className={styles.contact_btn} href="#">
+          Contact
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
