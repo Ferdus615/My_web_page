49166f6cbcf47ec08c9d86513c44a822b9aa7f67
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-05-17 23:52:43 +0600

    skills page

---
+++ my_web/src/pages/Skills.jsx
@@ -0,0 +1,15 @@
+const Skills = () => {
+  return (
+    <div className="skills">
+      <h1>Skills</h1>
+      <p>Here are some of my skills:</p>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>CSS</li>
+        <li>HTML</li>
+      </ul>
+    </div>
+  );
+}
