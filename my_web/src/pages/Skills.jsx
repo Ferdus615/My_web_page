87046bdf8a88fa0c65ac0069b9eec5912592ac28
Ferdus Rahman <88364746+Ferdus615@users.noreply.github.com>
2025-05-21 02:00:14 +0600

    Skills page grid

--- my_web/src/pages/Skills.jsx
+++ my_web/src/pages/Skills.jsx
@@ -2,15 +2,16 @@ import styles from "./Skills.module.css";
 
 const Skills = () => {
   return (
-    <div className={styles.skills}>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Pyhton</li>
-        <li>React</li>
-        <li>Node.js</li>
-      </ul>
+    <div className={styles.parent}>
+      <div class={styles.div1}>Technologies & Skills</div>
+      <div class={styles.div2}>JavaScript</div>
+      <div class={styles.div3}>HTML</div>
+      <div class={styles.div4}>CSS</div>
+      <div class={styles.div5}>Python</div>
+      <div class={styles.div6}>React</div>
+      <div class={styles.div7}>Django</div>
+      <div class={styles.div8}>Node.js</div>
+      <div class={styles.div9}>C/C++</div>
     </div>
   );
 };
